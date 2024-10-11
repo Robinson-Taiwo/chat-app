94529aec009a2f191f5abe1b59ce6229df05825e
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2024-10-11 10:14:11 +0100

    styling

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,20 +1,35 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-var-requires */
-const express = require("express");
+const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
-const server = (0, http_1.createServer)();
+const app = (0, express_1.default)();
+const server = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3000", // Frontend running on port 3000
         credentials: true,
         methods: ["POST", "GET"],
-    }
+    },
 });
 io.on("connection", (socket) => {
-    io.emit("message", "Hello User", socket.id);
+    console.log("A user connected: ", socket.id);
+    // Listen for the message and userName from the client
+    socket.on("sendMessage", (data) => {
+        const { message, userName } = data;
+        console.log(`Message received from ${userName}: ${message}`);
+        // Broadcast the message along with the userName
+        io.emit("receiveMessage", { message, userName });
+    });
+    // When the user disconnects
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
 });
 server.listen(4000, () => {
-    console.log("server started on PORT", 4000);
+    console.log("Server started on PORT", 4000);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const express = require("express")
-import {Server} from "socket.io"
+import express from "express";
+import { Server } from "socket.io";
 import { createServer } from "http";
-const server = createServer()
- 
+const app = express();
+const server = createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:3000",
-        credentials:true,
-        methods:["POST", "GET"],
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Frontend running on port 3000
+    credentials: true,
+    methods: ["POST", "GET"],
+  },
+});
 
-io.on("connection", (socket)=>{
-    io.emit("message","Hello User", socket.id)
-})
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
 
-server.listen(4000,()=>{
-    console.log("server started on PORT",4000)
-}) 
+  // Listen for the message and userName from the client
+  socket.on("sendMessage", (data) => {
+    const { message, userName } = data;
+    console.log(`Message received from ${userName}: ${message}`);
+
+    // Broadcast the message along with the userName
+    io.emit("receiveMessage", { message, userName });
+  });
+
+  // When the user disconnects
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server started on PORT", 4000);
+});
